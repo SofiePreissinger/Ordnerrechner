bf4ce31247de3cc5df38e82fef41b4a5a1b5723b
SofiePreissinger <132834005+SofiePreissinger@users.noreply.github.com>
2023-08-05 11:15:37 +0200

    Update Ordnerrechner.py

--- Ordnerrechner.py
+++ Ordnerrechner.py
@@ -1,13 +1,19 @@
 import os
 
 def get_directory_size(pfad):
+    entries = os.scandir(pfad)
+    sorted_entires = sorted (entries, key=lambda entry: entry.stat().st_size, reverse=True)
     total_size = 0
-    with os.scandir(pfad) as entries:
-        for entry in entries:
-            if entry.is_file():
-                total_size += entry.stat().st_size
-            elif entry.is_dir():
-                total_size += get_directory_size(entry.path)
+
+    try:
+        with os.scandir(pfad) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    total_size += entry.stat().st_size
+                elif entry.is_dir():
+                    total_size += get_directory_size(entry.path)
+    except OSError as e:
+        print(f"Error: {e}")
     return total_size
 
 def format_size(size):
@@ -17,15 +23,23 @@ def format_size(size):
         size /= 1024
 
 def print_directory_sizes(pfad):
-    total_size = get_directory_size(pfad)
-    print(f"{pfad} - {format_size(total_size)}")
-    with os.scandir(pfad) as entries:
-        for entry in entries:
-            if entry.is_dir():
-                size = get_directory_size(entry.path)
-                print(f"{entry.path} - {format_size(size)}")
+    try:
+        size = get_directory_size(pfad)
+        print(f"{pfad} - {format_size(size)}")
+        with os.scandir(pfad) as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    size = get_directory_size(entry.path)
+                    print(f"{entry.path} - {format_size(size)}")
+    except OSError as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    target_directory = input("Bitte geben Sie den Dateipfad eines Orderners ein: ")
-    print("Ordner wird gescannt...\n")
-    print_directory_sizes(target_directory)
+    try:
+        target_directory = input("Bitte geben Sie den Dateipfad eines Orderners ein: ")
+        print("Ordner wird gescannt...\n")
+        print_directory_sizes(target_directory)
+    except KeyboardInterrupt:
+        print("Scann vom Benutzer unterbrochen.")
+    except Exception as e:
+        print(f"Ein unerwarteter Fehler ist aufgetreten.")
